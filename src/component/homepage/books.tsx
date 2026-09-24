ed2d8b2fba5bbca900c5b9b36b8793d0bab52d80
mdshahariar98 <mdsidratulsaimshccho@gmail.com>
2026-09-24 14:08:00 +0600

    deployment problem

--- src/component/homepage/books.tsx
+++ src/component/homepage/books.tsx
@@ -2,28 +2,34 @@ import { IdataType } from '@/type/page';
 import React from 'react';
 import BookCard from '../selected/BookCard';
 
-const getBookData = async():Promise<IdataType[]> =>{
-    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`);
-    
-    return res.json()
-}
+const getBookData = async (): Promise<IdataType[]> => {
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SERVER_BASE_URL ||
+    `https://${process.env.VERCEL_URL}`;
 
-const BooksPage = async() => {
+  const res = await fetch(`${baseUrl}/booksData.json`);
 
-    const books:IdataType[] = await getBookData();
+  if (!res.ok) {
+    throw new Error('Failed to fetch books data');
+  }
 
-    return (
-        <section className=' container mx-auto text-center mt-25'>
-            <p className=' text-5xl font-bold mb-9'>Books</p>
+  return res.json();
+};
 
-            <div className=' grid grid-cols-3 gap-6 mb-28'>
-                {
-                    books.map((book, ind)=> <BookCard key={ind} book={book} />)
-                }
-            </div>
+const BooksPage = async () => {
+  const books: IdataType[] = await getBookData();
 
-        </section>
-    );
+  return (
+    <section className="container mx-auto text-center mt-25">
+      <p className="text-5xl font-bold mb-9">Books</p>
+
+      <div className="grid grid-cols-3 gap-6 mb-28">
+        {books.map((book, ind) => (
+          <BookCard key={ind} book={book} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default BooksPage;
